69f5aca91a74fdf69c84f0725c6093342125e6a3
齐英华 <>
2018-12-31 14:27:53 +0800

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
 # CSI-Tool-Camera-Shooting
+
+
+# What is it used for?
+
+**This program is based on Linux 802.11n CSI Tool. This program automatically takes a photo with the camera and attaches the current CSI Packet ID to the photo. These photos are saved in a folder with the same name as your CSI data file.**
+## Screenshot
+<img src="pic1.png" width = "100%" />
+
+## Detail
+<img src="pic2.png" width = "100%" />
+<img src="opencv-logo.png" width = "30%" />
+
+# Install
+This code is based on Linux 802.11n CSI Tool http://dhalperi.github.io/linux-80211n-csitool/ . Make sure you have installed it and can use it properly.
+
+**Download this project and install Opencv firstly.If you have already installed Opencv, go directly to Step 6.If you fail to install Opencv according to Step1-Step5, please find other installation help on the Internet. Please make sure you have successfully installed Opencv before running Step6.**
+
+Step 1. Install package
+~~~
+sudo apt-get install build-essential
+sudo apt-get install libgtk2.0-dev
+sudo apt-get install pkg-config 
+sudo apt-get install cmake
+~~~
+
+Step 2. Unzip Opencv
+
+&nbsp;&nbsp;&nbsp;Run commands under `CSI-Tool-Camera-Shooting` folder:
+~~~
+unzip opencv-2.4.13.6.zip
+cd opencv-2.4.13.6/install
+~~~
+
+Step 3. Compile and install Opencv
+~~~
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+cmake ..
+make
+sudo make install
+~~~
+
+Step 4. Configuration Opencv
+~~~
+sudo gedit /etc/ld.so.conf.d/opencv.conf
+~~~
+&nbsp;&nbsp;&nbsp;Then add the `/usr/local/lib` command to the file.
+~~~
+sudo ldconfig
+sudo gedit /etc/bash.bashrc
+~~~
+&nbsp;&nbsp;&nbsp;Then add the following command to the end of file:
+
+>`PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig`
+
+>`export PKG_CONFIG_PATH`
+
+Step 5. Reboot and finish install Opencv
+~~~
+sudo reboot
+~~~
+
+Step 6. Compile User APP [ For Linux 802.11n CSI Tool ]
+
+&nbsp;&nbsp;&nbsp;Use the `netlink` folder in this code replace to `linux-80211n-csitool-supplementary/netlink` folder.
+
+&nbsp;&nbsp;&nbsp;Run commands under `linux-80211n-csitool-supplementary/netlink` folder:
+~~~
+make
+~~~
+
+# Usage
+
+>Parameter: [Camera ID] [Pic Save Interval] [Whether Auto Exit]
+
+>[Camera ID]: This parameter controls which camera to use when the computer has multiple cameras.When set to `0`, the program will use the first camera.When set to `1`, the program will use the second camera. And so on.
+
+>[Pic Save Interval]: This parameter controls the speed at which images are saved. When set to `0`, the program will save each frame of the camera. When set to `1`, the program will save an image every other frame. And so on.
+
+>[Whether Auto Exit]: This parameter controls whether the program automatically exits when CSI collect stops.When set to `0`,This program will always run. When set to `1`,This program will automatically exit when no CSI is acquired within 1 second.
+
+
+&nbsp;&nbsp;&nbsp;So,Run commands under `linux-80211n-csitool-supplementary/netlink` folder:
+~~~
+./camera 0 0 1
+~~~
+&nbsp;&nbsp;&nbsp;Then,Start CSI collection  program.The camera program will automatically create a new folder with the same name as your CSI data file and save the image to this folder.
+~~~
+sudo ./log_to_file test.dat
+~~~
+
+>Warning 1: You must open or restart camera program firstly,Then start running the CSI collect program.
+
+>Warning 2: The program will automatically create a folder to save the image, but cannot be the same as the existing folder name.Otherwise, the program will report an error and automatically exit.
+
+>Warning 3: Not all cameras work properly under Linux .
---
+++ netlink/log_to_file.c
@@ -0,0 +1,184 @@
+#include "iwl_connector.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include<errno.h>
+#include<time.h>
+
+#define MAX_PAYLOAD 2048
+#define SLOW_MSG_CNT 1
+
+int sock_fd = -1;							
+FILE* out = NULL;
+typedef struct _MSG
+{
+    char path[64];
+    int count;
+}MSG;
+int    shmid;
+int    ret;
+MSG *p = NULL;
+
+void check_usage(int argc, char** argv);
+FILE* open_file(char* filename, char* spec);
+void caught_signal(int sig);
+void exit_program(int code);
+void exit_program_err(int code, char* func);
+
+int main(int argc, char** argv)
+{
+	/* Local variables */
+	struct sockaddr_nl proc_addr, kern_addr;	// addrs for recv, send, bind
+	struct cn_msg *cmsg;
+	char buf[4096];
+	//int ret;
+	unsigned short l, l2;
+	int count = 0;
+    shmid = shmget(0x5120, 0, 0);
+    if (shmid == -1)
+    {
+        perror("you must open or restart camera program firstly !!!\nipcs");
+        return errno;
+    }
+    p = shmat(shmid, NULL, 0);
+    if (p == (void *)-1 )
+    {
+        perror("shmget err");
+        return errno;
+    }
+
+	/* Make sure usage is correct */
+	check_usage(argc, argv);
+
+	/* Open and check log file */
+	out = open_file(argv[1], "w");
+	strcpy(p->path, argv[1]);
+
+	/* Setup the socket */
+	sock_fd = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_CONNECTOR);
+	if (sock_fd == -1)
+		exit_program_err(-1, "socket");
+
+	/* Initialize the address structs */
+	memset(&proc_addr, 0, sizeof(struct sockaddr_nl));
+	proc_addr.nl_family = AF_NETLINK;
+	proc_addr.nl_pid = getpid();			// this process' PID
+	proc_addr.nl_groups = CN_IDX_IWLAGN;
+	memset(&kern_addr, 0, sizeof(struct sockaddr_nl));
+	kern_addr.nl_family = AF_NETLINK;
+	kern_addr.nl_pid = 0;					// kernel
+	kern_addr.nl_groups = CN_IDX_IWLAGN;
+
+	/* Now bind the socket */
+	if (bind(sock_fd, (struct sockaddr *)&proc_addr, sizeof(struct sockaddr_nl)) == -1)
+		exit_program_err(-1, "bind");
+
+	/* And subscribe to netlink group */
+	{
+		int on = proc_addr.nl_groups;
+		ret = setsockopt(sock_fd, 270, NETLINK_ADD_MEMBERSHIP, &on, sizeof(on));
+		if (ret)
+			exit_program_err(-1, "setsockopt");
+	}
+
+	/* Set up the "caught_signal" function as this program's sig handler */
+	signal(SIGINT, caught_signal);
+
+	/* Poll socket forever waiting for a message */
+	while (1)
+	{
+		/* Receive from socket with infinite timeout */
+		ret = recv(sock_fd, buf, sizeof(buf), 0);
+		if (ret == -1)
+			exit_program_err(-1, "recv");
+		/* Pull out the message portion and print some stats */
+		cmsg = NLMSG_DATA(buf);
+		if (count % SLOW_MSG_CNT == 0)
+			printf("received %d bytes: id: %d val: %d seq: %d clen: %d\n", cmsg->len, cmsg->id.idx, cmsg->id.val, cmsg->seq, cmsg->len);
+		/* Log the data to file */
+		l = (unsigned short) cmsg->len;
+		l2 = htons(l);
+		fwrite(&l2, 1, sizeof(unsigned short), out);
+		ret = fwrite(cmsg->data, 1, l, out);
+		if (count % 100 == 0)
+			printf("wrote %d bytes [msgcnt=%u]\n", ret, count);
+		++count;
+		//strcpy(p->path, "pic");
+        p->count = count;
+		if (ret != l)
+			exit_program_err(1, "fwrite");
+	}
+
+	exit_program(0);
+	return 0;
+}
+
+void check_usage(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <output_file>\n", argv[0]);
+		exit_program(1);
+	}
+}
+
+FILE* open_file(char* filename, char* spec)
+{
+	FILE* fp = fopen(filename, spec);
+	if (!fp)
+	{
+		perror("fopen");
+		exit_program(1);
+	}
+	return fp;
+}
+
+void caught_signal(int sig)
+{
+	fprintf(stderr, "Caught signal %d\n", sig);
+	exit_program(0);
+}
+
+void exit_program(int code)
+{
+	if (out)
+	{
+		fclose(out);
+		out = NULL;
+	}
+	if (sock_fd != -1)
+	{
+		close(sock_fd);
+		sock_fd = -1;
+	}
+	shmdt(p);   
+    printf("\nunlock thread\n");
+	ret=shmctl(shmid, IPC_RMID, NULL);
+    if (ret < 0)
+    {
+       perror("error in delete ram\n");
+    }
+	exit(code);
+}
+
+void exit_program_err(int code, char* func)
+{
+	shmdt(p);   
+    printf("\nunlock thread\n");
+	ret=shmctl(shmid, IPC_RMID, NULL);
+    if (ret < 0)
+    {
+       perror("error in delete ram\n");
+    }
+
+	perror(func);
+	exit_program(code);
+}
+
